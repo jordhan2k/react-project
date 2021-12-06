b4fce6ae15c948b10e47bdd810d632f9dfa4d209
Jordan Hwang <dunghoang1c.hanu@gmail.com>
2021-12-06 11:11:03 +0700

    Add event handlers + AddPanel comp.

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react'
 import styled from 'styled-components';
+import AddPanel from '../components/AddPanel';
 import Card from '../components/Card';
-
-import { color, status, } from '../utils/constants';
+import { color } from '../utils/constants';
+import { v4 as uuidv4 } from 'uuid';
 
 
 const Container = styled.div`
@@ -13,12 +14,21 @@ const Container = styled.div`
 `;
 
 const Wrapper = styled.div`
-    width: 25vw;
+    width: 400px;
     height: 100vh;
     display: flex;
     flex-direction: column;
     align-items: center;
     box-shadow: 0 0 1px 10px rgba(0, 0 , 0, 0, .8);
+`;
+
+
+
+
+const Title = styled.h1`
+    font-size: 25px;
+    font-weight: 600;
+    margin: 20px 0;
 `;
 
 
@@ -30,77 +40,54 @@ export default class Dashboard extends Component {
         super();
 
         this.state = {
-            todos: [
-                {
-                    _id: "1",
-                    title: "Learn React",
-                    desc: "lorem ipsum",
-                    status: status.OPEN
-                },
-
-                {
-                    _id: "2",
-                    title: "Research MERN stack",
-                    desc: "lorem ipsum",
-                    status: status.OPEN
-                },
-
-                {
-                    _id: "3",
-                    title: "Team up",
-                    desc: "lorem ipsum",
-                    status: status.COMPELETED
-                },
-
-                {
-                    _id: "4",
-                    title: "Learn React",
-                    desc: "lorem ipsum",
-                    status: status.OPEN
-                },
-
-                {
-                    _id: "5",
-                    title: "Research MERN stack",
-                    desc: "lorem ipsum",
-                    status: status.OPEN
-                },
-
-                {
-                    _id: "6",
-                    title: "Team up",
-                    desc: "lorem ipsum",
-                    status: status.COMPELETED
-                },
-                {
-                    _id: "7",
-                    title: "Research MERN stack",
-                    desc: "lorem ipsum",
-                    status: status.OPEN
-                },
-
-                {
-                    _id: "8",
-                    title: "Team up",
-                    desc: "lorem ipsum",
-                    status: status.COMPELETED
-                },
-            ],
-
+            todos: [],
         }
     }
+
+    changeStatus(todoId) {
+        this.setState(prevState => ({
+            ...prevState,
+            todos: prevState.todos.map(todo => todo._id === todoId ? { ...todo, isCompleted: !todo.isCompleted } : todo)
+        }));
+    }
+
+    addTodo(todo) {
+        this.setState({
+            todos: [
+                ...this.state.todos,
+                {
+                    ...todo,
+                    _id: uuidv4(),
+                    isCompleted: false
+                }
+            ]
+        })
+    }
+
+    deleteTodo(todoId) {
+        this.setState({
+            todos: this.state.todos.filter(todo => todo._id !== todoId)
+        })
+
+    }
+
+
 
     render() {
         return (
             <Container>
                 <Wrapper >
-
-                    {this.state.todos.map(item => (
-                        <Card key={item._id} todo={item} />
+                    <Title>My todo list</Title>
+                    <AddPanel addTodoHandler={this.addTodo.bind(this)} />
+                    {this.state.todos.length > 0 && this.state.todos.map(item => (
+                        <Card
+                            key={item._id}
+                            todo={item}
+                            statusHandler={this.changeStatus.bind(this)}
+                            deleteHandler={this.deleteTodo.bind(this)}
+                        />
                     ))}
                 </Wrapper>
-
-
             </Container>
         )
     }
